3155e35c3e9cc3578a27d15659f287e04fc5d8c9
Marconi <marconi@marconicivitavecchia.it>
2020-01-21 10:20:24 +0100

    idk

--- benchmark.py
+++ benchmark.py
@@ -1,14 +1,19 @@
-#crea una lista
 from timeit import time
 from random import randrange
 from selection_sort import selection_sort
-#...
-lylist = [randrange(1000) for idx in range(10000)]
-#calcola i tempi del selection
 
-#calcola i tempi del bubble
+#crea una lista
+
+lylist = [randrange(1000) for idx in range(10000)]
+
+#calcola i tempi del selection
 
 selection_sort_time = selection_sort(mytime.copy())
 
+#calcola i tempi del bubble
 
 bubble_sort_time = bubble_sort(mytime.copy())
+
+
+copy_list = mylist.copy()
+default_time = mylist.sort()
